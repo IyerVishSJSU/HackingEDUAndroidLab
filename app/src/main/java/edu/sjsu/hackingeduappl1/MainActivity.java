fd038a492f651bb1d37c1e643ebfc3742d2b1ff5
IyerVishSJSU <vishwanath.iyer@sjsu.edu>
2016-10-15 16:22:30 -0700

    Add Watson API feature

--- app/src/main/java/edu/sjsu/hackingeduappl1/MainActivity.java
+++ app/src/main/java/edu/sjsu/hackingeduappl1/MainActivity.java
@@ -1,33 +1,79 @@
 package edu.sjsu.hackingeduappl1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+//adding Watson Developer Cloud SDK for Java:
+import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.DocumentSentiment;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    //EditText editText;
+    EditText editText;
     Button button;
-    CheckBox chkBox;
+    String sentiment;
+
+    private class AskWatsonTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... textsToAnalyse) {
+
+            System.out.println(editText.getText());
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText("what is happening inside a thread - we are running Watson AlchemyAPI");
+                }
+            });
+            AlchemyLanguage service = new AlchemyLanguage();
+            service.setApiKey("7353b733912ca2a11993235e0906d65bd47ab055");
+
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put(AlchemyLanguage.TEXT, editText.getText());
+            DocumentSentiment sentiment = service.getSentiment(params).execute();
+            System.out.println(sentiment);
+
+            //passing the result to be displayed at UI in the main tread
+            return sentiment.getSentiment().getType().name();
+        }
+
+        //setting the value of UI outside of the thread
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText("The message's sentiment is: " + result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView=(TextView)findViewById(R.id.textView);
-        //editText=(EditText)findViewById(R.id.editText);
-        button= (Button)findViewById(R.id.button);
+        //initialize UI parameters
+        textView = (TextView) findViewById(R.id.textView2);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new textView.OnClickListener(){
-            public void OnClick(View v){
-                System.out.println("button was pressed");
+        //fire action when button is pressed
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Logging to the console that the button pressed for the text : " + editText.getText());
+                textView.setText("Displaying at UI the sentiment to be checked for : " + editText.getText());
+
+                AskWatsonTask task = new AskWatsonTask();
+                task.execute(new String[]{});
             }
-        };
 
-    };
-}
+        });
+
+
+    }}
